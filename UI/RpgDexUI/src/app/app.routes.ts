9f8d92483727dfab1d00cf186940387454d5ff35
vitorgilhi <vgilhidasilva@gmail.com>
2026-03-30 10:17:45 -0300

    feat: implement login and register layouts with responsive design

--- UI/RpgDexUI/src/app/app.routes.ts
+++ UI/RpgDexUI/src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home';
+import { UserLoginComponent } from './pages/user-login/user-login.component';
+import { UserRegisterComponent } from './pages/user-register/user-register.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent }, 
   { path: 'home', component: HomeComponent },
+  { path: 'login', component: UserLoginComponent },
+  { path: 'cadastro', component: UserRegisterComponent }
 ];
