a4da93c98681403144b067ed50fdd7d3f22a0f3f
vitorgilhi <vgilhidasilva@gmail.com>
2026-04-03 16:34:59 -0300

    feat: add campaigns dashboard with mock data and creation modal

--- UI/RpgDexUI/src/app/app.routes.ts
+++ UI/RpgDexUI/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home';
 import { UserLoginComponent } from './pages/user-login/user-login.component';
 import { UserRegisterComponent } from './pages/user-register/user-register.component';
+import { CampaignsComponent } from './pages/campaigns/campaigns';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent }, 
   { path: 'home', component: HomeComponent },
   { path: 'login', component: UserLoginComponent },
-  { path: 'cadastro', component: UserRegisterComponent }
+  { path: 'cadastro', component: UserRegisterComponent },
+  { path: 'campanhas', component: CampaignsComponent}
 ];
---
+++ UI/RpgDexUI/src/app/pages/campaigns/campaigns.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+export interface Campaign {
+  id: number;
+  name: string;
+  imageUrl: string;
+}
+
+@Component({
+  selector: 'app-campaigns',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './campaigns.html',
+  styleUrls: ['./campaigns.css']
+})
+export class CampaignsComponent {
+  // Mock Data
+  myCampaigns: Campaign[] = [
+    { 
+      id: 1, 
+      name: 'A Mina Perdida', 
+      imageUrl: 'https://placehold.co/400x400/e0e0e0/9E74D0?text=Campanha+1' 
+    },
+    { 
+      id: 2, 
+      name: 'Maldição de Strahd', 
+      imageUrl: 'https://placehold.co/400x400/e0e0e0/9E74D0?text=Campanha+2' 
+    }
+  ];
+
+  isModalOpen = false;
+
+  toggleModal() {
+    this.isModalOpen = !this.isModalOpen;
+  }
+}
