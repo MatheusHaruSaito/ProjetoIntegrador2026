dd513cb162f823aa6b1c101649af2a423507b694
vitorgilhi <vgilhidasilva@gmail.com>
2026-04-10 19:11:23 -0300

    Sincronizando arquivos manuais antes do merge

--- UI/RpgDexUI/src/app/app.routes.ts
+++ UI/RpgDexUI/src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { HomeComponent } from './pages/home/home';
 import { UserLoginComponent } from './pages/user-login/user-login.component';
 import { UserRegisterComponent } from './pages/user-register/user-register.component';
 import { CharacterList } from './pages/character-list/character-list';
+import { CampaignsComponent } from './pages/campaigns/campaigns';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent }, 
   { path: 'home', component: HomeComponent },
   { path: 'login', component: UserLoginComponent },
   { path: 'cadastro', component: UserRegisterComponent },
-  { path: 'character', component: CharacterList }
+  { path: 'character', component: CharacterList },
+  { path: 'campaign', component: CampaignsComponent }
 
 ];
