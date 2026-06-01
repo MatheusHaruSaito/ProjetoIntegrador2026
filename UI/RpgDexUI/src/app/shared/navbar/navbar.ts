f38876da1929c0e1c76ddb864bfa617396f56fa6
MatheusHaruSaito <matheusharusaito@gmail.com>
2026-05-31 21:50:44 -0300

    fixed Profile Icon

--- UI/RpgDexUI/src/app/shared/navbar/navbar.ts
+++ UI/RpgDexUI/src/app/shared/navbar/navbar.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { AuthService } from '../../services/auth-service';
@@ -14,16 +14,18 @@ export class NavbarComponent implements OnInit {
   isMenuOpen = false;
   userAvatarUrl: string = '';
 
-  constructor(public authService: AuthService) {}
+  constructor(public authService: AuthService, private cdr: ChangeDetectorRef) {}
 
   ngOnInit(): void {
     if (this.authService.isLoggedIn()) {
       this.authService.GetLoggedUser().subscribe({
         next: (response) => {
           this.userAvatarUrl = response.data?.iconPath ?? '';
+          this.cdr.detectChanges();
         },
         error: () => {
           this.userAvatarUrl = '';
+          this.cdr.detectChanges();
         }
       });
     }
