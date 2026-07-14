27bdae80e0d04655e831745c0a002026f376e1a0
MatheusHaruSaito <matheusharusaito@gmail.com>
2026-07-14 16:55:37 -0300

    Email Service

---
+++ API/RpgDex/RpgDex.Domain/Interfaces/IEmailService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RpgDex.Domain.Interfaces
+{
+    public interface IEmailService
+    {
+        Task<(bool isEmailSent, string message)> SendEmail(string receiverEmail, string receiverName, string subject, string htmlbody);
+    }
+}
---
+++ API/RpgDex/RpgDex.Infrastructure/Services/EmailService.cs
@@ -0,0 +1,49 @@
+﻿using MailKit.Net.Smtp;
+using Microsoft.Extensions.Options;
+using MimeKit;
+using RpgDex.Domain.Interfaces;
+using RpgDex.Infrastructure.Settings;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RpgDex.Infrastructure.Services
+{
+    public class EmailService : IEmailService
+    {
+        private readonly EmailSettings _settings;
+        public EmailService(IOptions<EmailSettings> options)
+        {
+            _settings = options.Value;
+        }
+        public async Task<(bool isEmailSent, string message)> SendEmail(string receiverEmail,string receiverName, string subject, string htmlBody)
+        {
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress("RpgDex", _settings.Username));
+            message.To.Add(new MailboxAddress(receiverName, receiverEmail));
+            message.Subject = subject;
+            message.Body = new TextPart("html")
+            {
+                Text = htmlBody
+            };
+
+            try
+            {
+                using (var client = new SmtpClient()) {
+                    await client.ConnectAsync(_settings.SmtpServer, _settings.SmtpPort, true);
+
+                    await client.AuthenticateAsync(_settings.Username, _settings.Password);
+
+                    await client.SendAsync(message);
+
+                    await client.DisconnectAsync(true);
+                }
+            }
+            catch(Exception ex) {
+                return (false, "Error sending email: " + ex.Message);
+            }
+
+            return (true, "Email sent successfully.");
+        }
+    }
+}
