054562f6dadfdbb412ddf27f24937311f59ca190
MatheusHaruSaito <matheusharusaito@gmail.com>
2026-08-14 07:57:18 -0300

    Added Online Api

--- UI/RpgDexUI/src/environments/environment.ts
+++ UI/RpgDexUI/src/environments/environment.ts
@@ -1,6 +1,6 @@
-const hostname = "localhost";
+const hostname = 'localhost';
 const port = 8080;
 export const environment = {
-    RpxDexApi: `http://${{hostname}}:${{port}}/api`,
+  // RpxDexApi: `http://${{hostname}}:${{port}}/api`,
+  RpxDexApi: `https://projetointegrador2026.onrender.com/api`,
 };
-    
