5eb6100c79bb8fc6c207e462e68e8155f303c218
MatheusHaruSaito <matheusharusaito@gmail.com>
2026-08-16 21:23:49 -0300

    fix UserService

--- API/RpgDex/RpgDex.WebApi/Controllers/UserController.cs
+++ API/RpgDex/RpgDex.WebApi/Controllers/UserController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using RpgDex.Application.Interfaces;
 using RpgDex.Application.Dto;
+using RpgDex.WebApi.Extensions;
 
 
 
@@ -19,49 +20,16 @@ namespace RpgDex.WebApi.Controllers
         }
 
         [HttpGet("{Id}")]
-        public async Task<ActionResult<UserResponse>> GetUserById(Guid Id)
+        public async Task<IActionResult> GetUserById(Guid Id)
         {
             var result = await _userService.GetUserById(Id);
-
-            if (result.IsFailure)
-            {
-                return NotFound(new
-                {
-                    success = result.IsSuccess,
-                    message = result.Error,
-                    data = result.Value
-                });
-            }
-
-            return Ok(new
-            {
-                success = result.IsSuccess,
-                data = result.Value
-            });
+            return result.ToIActionResult();
         }
         [HttpPut("{Id}")]
-        public async Task<ActionResult<string>> UpdateUserProfile(Guid Id,UpdateUserProfileDTO updateUserProfileDTO)
+        public async Task<IActionResult> UpdateUserProfile(Guid Id,UpdateUserProfileDTO updateUserProfileDTO)
         {
             var result = await _userService.UpdateUserProfileAsync(Id, updateUserProfileDTO);
-
-            if (result.IsFailure)
-            {
-                return NotFound(new
-                {
-                    success = result.IsSuccess,
-                    message = result.Error,
-                    data = result.Value
-                });
-            }
-            if (result.IsSuccess)
-            {
-                return Ok(new
-                {
-                    success = result.IsSuccess,
-                    data = result.Value
-                });
-            }
-            return StatusCode(500,"Unexpected Error has occurred");
+            return result.ToIActionResult();
         }
 
     }
