1f8b10bafd884f3c25542a2d329f845fa3fcf39d
MatheusHaruSaito <matheusharusaito@gmail.com>
2026-08-29 18:34:58 -0300

    fix environments

--- UI/RpgDexUI/src/environments/environment.development.ts
+++ UI/RpgDexUI/src/environments/environment.development.ts
@@ -1,7 +1,6 @@
 const hostname = 'localhost';
 const port = 8080;
 export const environment = {
-  // RpxDexApi: `http://${hostname}:${port}/api`,
-  RpxDexApi: `https://projetointegrador2026.onrender.com/api`,
+  RpxDexApi: `http://${hostname}:${port}/api`,
   GoogleClientId: '1087467635955-jd1c7b9tss2lpkpomrot7u1b81l4lpbe.apps.googleusercontent.com',
 };
