cca976cc482b7f412a1974ef3618ba2893b5297d
MatheusHaruSaito <matheusharusaito@gmail.com>
2026-09-12 19:54:25 -0300

    add: campaign chat front

---
+++ UI/RpgDexUI/src/app/components/campaign-chat-component/campaign-chat-component.ts
@@ -0,0 +1,57 @@
+import { ChangeDetectorRef, Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { ChatMessage } from '../../../models/chatMessage';
+import { Subscription } from 'rxjs';
+import { AuthService } from '../../services/auth-service';
+import { CampaignChatService } from '../../services/campaign-chat-service';
+import { CampaignChatMessageRequest } from '../../../models/campaignChatMessageRequest';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-campaign-chat-component',
+  imports: [CommonModule, FormsModule],
+  templateUrl: './campaign-chat-component.html',
+  styleUrl: './campaign-chat-component.css',
+})
+export class CampaignChatComponent implements OnInit, OnDestroy {
+  @Input() campaignId!: string;
+
+  messages: ChatMessage[] = [];
+  newMessageText: string = '';
+  private chatSubscription!: Subscription;
+
+  constructor(
+    private chatService: CampaignChatService,
+    private authService: AuthService,
+    private cdr: ChangeDetectorRef,
+  ) {}
+  ngOnInit(): void {
+    const token = this.authService.Token;
+    this.chatService.startConnection(this.campaignId, token);
+    this.chatSubscription = this.chatService.getMessages().subscribe((msg) => {
+      this.messages.push(msg);
+      this.cdr.detectChanges();
+    });
+  }
+
+  send(): void {
+    if (!this.newMessageText.trim()) return;
+    const request: CampaignChatMessageRequest = {
+      campaignId: this.campaignId,
+      message: this.newMessageText,
+    };
+
+    this.chatService.sendMessage(request).subscribe({
+      next: (r) => {
+        console.log(r);
+        this.newMessageText = '';
+      },
+      error: (err) => console.log('error sending message: ', err),
+    });
+    console.log(this.messages);
+  }
+
+  ngOnDestroy(): void {
+    (this.chatService.stopConnection(this.campaignId), this.chatSubscription.unsubscribe());
+  }
+}
